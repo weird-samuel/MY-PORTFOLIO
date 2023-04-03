4c2cca00626deb6cdb3692b7e77f82034c21fa73
samuel Adekunle <110774881+weird-samuel@users.noreply.github.com>
2023-04-03 18:41:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+window.addEventListener("scroll", function () {
+  var navbar = document.querySelector("nav");
+  if (window.scrollY > 100) {
+    navbar.style.backgroundColor = "#1f1f1f";
+  } else {
+    navbar.style.backgroundColor = "";
+  }
+});
+// 
+var tablinks = document.getElementsByClassName('tab-links');
+var tabcontents = document.getElementsByClassName('tab-contents');
+
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link")
+  }
+
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab")
+  }
+  event.currentTarget.classList.add('active-link');
+  document.getElementById(tabname).classList.add('active-tab')
+}
+// 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwUrzt_KtK27UusAbKooQ8AtGgR__ufk4UlCi5HVJfFZz-DSe8CSVqNlcIZLhNBj0rXAQ/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById('msg')
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => {
+      msg.innerHTML = "Message Sent!"
+      setTimeout(function () {
+        msg.innerHTML = ""
+      }, 5000)
+      form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
+
+////Page Load
+let stage = document.querySelector(".stage")
+let content = document.querySelector(".content")
+window.addEventListener("load", () => {
+    stage.style.opacity = "1";
+    setTimeout(() => {
+        stage.style.opacity = "0";
+        setTimeout(() => {
+            content.style.opacity = "0";
+            setTimeout(() => {
+              content.style.display = "none";
+              window.scrollTo(0,0)
+            }, 600)
+        }, 500)
+    }, 4500)
+})
